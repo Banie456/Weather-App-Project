e7bc997242c1a3a7edc94139d3dad376aa8b1aad
Banie456 <sinqobile.myeki@gmail.com>
2024-10-10 12:55:20 +0200

    added the weather projet

---
+++ src/script.js
@@ -0,0 +1,56 @@
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  let city = searchInputElement.value;
+  city = city.charAt(0).toUpperCase() + city.slice(1);
+
+  cityElement.innerHTML = city;
+  let apiKey = "631da3a424966be5280f2o0c487tf7f3";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateElement.innerHTML = formatDate(currentDate);
+
+function showWeather(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  console.log(temperature);
+
+  let temperatureValue = document.querySelector("#temperature-value");
+  temperatureValue.innerHTML = `${temperature}`;
+}
